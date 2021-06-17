ccb804ba5e170d599c0fd23a18224b06358e2f53
paolataboada <taboadapaola3@gmail.com>
2021-06-16 21:05:50 -0500

    agregando proyecto

--- src/index.js
+++ src/index.js
@@ -1,3 +1,75 @@
 import validator from './validator.js';
+//console.log(validator);
 
-console.log(validator);
+//Esta función se utilizará para verificar el valor ingresado
+const cardValidation = document.getElementById("cardValidation");
+function btnValidar() {
+  const cardNumber = document.getElementById("cardNumber").value;
+  if(cardNumber.length == 0){
+    alert("Introduzca los datos para continuar")
+    document.getElementById("cardNumber").focus();
+  } else {
+    return validator.isValid(cardNumber);
+  }
+}
+cardValidation.addEventListener("click", btnValidar);
+
+//Esta función guardará la información del usuario (por el momento aparecerá en un alert)
+const checkbox = document.getElementById("checkbox");
+function checkSaveInfo() {
+  const userInformation1 = document.getElementById("cardNumber").value;
+  const userInformation2 = document.getElementById("cardOwner").value;
+  const userInformation3 = document.getElementById("expirationDate").value;
+  const userInformation4 = document.getElementById("securityCode").value;
+  if(userInformation1.length>0 || userInformation2.length>0 || userInformation3.length>0 || userInformation4.length>0){
+    alert("Información: Tarjeta " + userInformation1 + " Nombre " + userInformation2 + " Fecha "+userInformation3 + " CVV " + userInformation4);
+    return true;
+  } else {
+    alert("No se ha encontrado información");
+    return false;
+  }
+}
+checkbox.addEventListener("click", checkSaveInfo);
+
+//Esta función mostrará la segunda página o sección de pago
+const continueProcess = document.getElementById("continueProcess");
+const general = document.getElementById("general");
+function btnContinuar() {
+  const paymentSection = document.getElementById("pageTwo");
+  const userInformation1 = document.getElementById("cardNumber").value;
+  const userInformation2 = document.getElementById("cardOwner").value;
+  const userInformation3 = document.getElementById("expirationDate").value;
+  const userInformation4 = document.getElementById("securityCode").value;
+  if(userInformation1.length>0 || userInformation2.length>0 || userInformation3.length>0 || userInformation4.length>0){
+    return paymentSection.style.display="", general.style.display="none";
+  } else {
+    alert("Debe ingresar la información solicitada")
+  }
+}
+continueProcess.addEventListener("click", btnContinuar);
+
+//Esta función esconderá los últimos dígitos de la tarjeta
+const ocultarTarjeta = document.getElementById("ocultarTarjeta");
+function btnOcultar(){
+  const cardNumber = document.getElementById("cardNumber").value;
+  let finalNumber = document.getElementById("nextPage");
+  let finalNumberContent = document.createElement("li");
+  finalNumberContent.innerHTML = "Pagar con: " + validator.maskify(cardNumber);
+  finalNumber.insertAdjacentElement("afterbegin", finalNumberContent);
+  
+  let finalSubtotal = document.getElementById("nextPage");
+  let finalSubtotalContent = document.createElement("li");
+  finalSubtotalContent.innerHTML = "Subtotal: s/. 250.00";
+  finalSubtotal.insertAdjacentElement("beforeend", finalSubtotalContent);
+  
+  let finalShipping = document.getElementById("nextPage");
+  let finalShippingContent = document.createElement("li");
+  finalShippingContent.innerHTML = "Envío: s/. 30.00" ;
+  finalShipping.insertAdjacentElement("beforeend", finalShippingContent);
+
+  let finalTotal = document.getElementById("nextPage");
+  let finalTotalContent = document.createElement("li");
+  finalTotalContent.innerHTML = "Total: s/. 280.00 ";
+  finalTotal.insertAdjacentElement("beforeend", finalTotalContent);
+}
+ocultarTarjeta.addEventListener("click",btnOcultar);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,32 @@
 const validator = {
-  // ...
-};
+  isValid: function validacion(cardNumber){
+    let arrayTarjeta = Array.from(cardNumber);
+    arrayTarjeta = arrayTarjeta.map(Number);
+    let numInvertido = arrayTarjeta.reverse();
+    let posicionPar = numInvertido.filter((num, i) => i%2!==0);
+    let posicionImpar = numInvertido.filter((num, i) => i%2==0);
+    let posicionParx2 = posicionPar.map(num => num*2);
+    let productoMayor9 = posicionParx2.filter(num => num>9).map(num => num-9);
+    let productoMenor9 = posicionParx2.filter(num => num<10);
+    let sumaPar1 = productoMayor9.reduce(((a, b) => a+b), 0);
+    let sumaPar2 = productoMenor9.reduce(((a, b) => a+b), 0);
+    let sumaImpar = posicionImpar.reduce(((a, b) => a+b),0);
+    const sumaTotal = sumaPar1 + sumaPar2 + sumaImpar;
+    if (sumaTotal%10===0){
+      alert ("La tarjeta ha sido verificada correctamente.");
+      return true;
+    } else {
+      alert ("El número introducido no es válido.");
+      return false;
+    }
+  },
 
+  maskify: function enmascaramiento(cardNumber){
+    let cuatroDigitos = cardNumber.slice(-4);
+    let ocultarDigitos = cardNumber.slice(0,-4);
+    let enmascarar = ocultarDigitos.replace(ocultarDigitos,"#").repeat(ocultarDigitos.length) + cuatroDigitos;
+      alert ("Pagar con " + enmascarar);
+      return enmascarar;
+    }
+};
 export default validator;
